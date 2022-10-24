c6ad8854ad8e2307c14ce1e1efec9837b0a7d254
Godnelson <luizgatohd@gmail.com>
2022-10-24 00:34:05 -0300

    request started

--- app/src/main/java/com/primero/boschmaps/MainActivity.kt
+++ app/src/main/java/com/primero/boschmaps/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.primero.boschmaps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener(
+            object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    run()
+                }
+
+            }
+        )
+
+
+    }
+
+    private val client = OkHttpClient()
+
+    fun run() {
+        val edv = findViewById<EditText>(R.id.editTextTextPersonName).text.toString().toInt()
+        val password = findViewById<EditText>(R.id.editTextTextPassword).text.toString()
+
+        val formBody = FormBody.Builder()
+            .add("EDV", edv.toString())
+            .add("Password", password.toString())
+            .build()
+
+        val request = Request.Builder()
+            .url("http://10.0.2.2:5000")
+            .addHeader("Content-Type","application/json")
+            .post(formBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                println(response.body!!.string())
+                println(formBody)
+            }
+
+        })
     }
 }
